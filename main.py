a51eb5820bd804dc5fe75b9eaf4f3b8fbf1c2f97
nhi-batau <ssmemes163@gmail.com>
2025-03-09 12:58:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from logging.handlers import RotatingFileHandler
 
 # Config 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7535024319:AAFbk6M1WuFQMEhqGsE2B9mg7VzXutJ1UW8")
-    API_ID = int(os.environ.get("API_ID",  "22100695"))
-    API_HASH = os.environ.get("API_HASH", "0e8f93300ccbbcd56066e6d790b0d3b2")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7984555687:AAHfslBio4f7_nm9-ucZMH3epeLaJDtGv88")
+    API_ID = int(os.environ.get("API_ID",  "20114039"))
+    API_HASH = os.environ.get("API_HASH", "87297b8f3cc8fc9bbce591ad30da5896")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     SESSIONS = "./SESSIONS"
     
